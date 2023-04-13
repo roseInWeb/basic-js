2414d178cbd488eb28c05afcf6d9d0f271ffbbf4
roseInWeb <i.a.semyonova@mail.ru>
2023-04-13 22:32:25 +0400

    fix: carbon-dating task is done

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,11 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity == 'string' && +sampleActivity !== 0 && +sampleActivity > 0 && +sampleActivity < 15 && !isNaN(+sampleActivity)) {
+    return Math.ceil(Math.log(MODERN_ACTIVITY / +sampleActivity) * (HALF_LIFE_PERIOD / 0.693));
+  }
+  return false;
 }
 
 module.exports = {
