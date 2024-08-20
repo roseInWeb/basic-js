0b9c8697eaa18048c10cbb6e8d4d82930d70c394
RoseInWeb <e.a.s.catinweb13@gmail.com>
2024-08-20 18:14:09 +0400

    fix: vigenere-cipher task is done

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,78 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor (type) {
+    this.type = type;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(str, key) {
+    if (!str || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    const abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let keyStr = ''.padStart(str.length, key).toUpperCase();
+    let res = '';
+    str = str.toUpperCase();
+
+    keyStr = keyStr.split('');
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === ' ') {
+        keyStr.splice(i, 0 , ' ');
+      }
+    }
+    keyStr.length = str.length;
+    keyStr = keyStr.join('');
+
+    for (let i = 0; i < str.length; i++) {
+      if (/^\p{L}$/u.test(str[i])) {
+        let index = abc.indexOf(str[i]) + abc.indexOf(keyStr[i]);
+        res += abc[index % 26];
+      } else {
+        res += str[i];
+      }
+    }
+
+    res = this.type === false ? res.split('').reverse().join('') : res;
+
+    return res;
+  }
+  decrypt(str, key) {
+    if (!str || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    const abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let keyStr = ''.padStart(str.length, key).toUpperCase();
+    let res = '';
+    str = str.toUpperCase();
+
+    keyStr = keyStr.split('');
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === ' ') {
+        keyStr.splice(i, 0 , ' ');
+      }
+    }
+    keyStr.length = str.length;
+    keyStr = keyStr.join('');
+
+    for (let i = 0; i < str.length; i++) {
+      if (/^\p{L}$/u.test(str[i])) {
+        let index = 0;
+        if (abc.indexOf(str[i]) - abc.indexOf(keyStr[i]) < 0) {
+          index =  26 + abc.indexOf(str[i]) - abc.indexOf(keyStr[i]);
+        } else {
+          index = abc.indexOf(str[i]) - abc.indexOf(keyStr[i]);
+        }
+        res += abc[index];
+      } else {
+        res += str[i];
+      }
+    }
+
+    res = this.type === false ? res.split('').reverse().join('') : res;
+
+    return res;
   }
 }
 
