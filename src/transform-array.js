3d8c144232cfa2cc016293cd5a573994d3ff344e
RoseInWeb <e.a.s.catinweb13@gmail.com>
2024-08-22 13:07:04 +0400

    fix: transform-array task is done

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("\'arr\' parameter must be an instance of the Array!");
+  }
+
+  const array = Array.from(arr);
+
+  for (let i = 0; i < arr.length; i++) {
+    if (array[i] === '--discard-next') {
+      array.splice(i, 2, false);
+    }
+
+    if (array[i] === '--discard-prev') {
+      if (i > 0) {
+        array.splice(i - 1, 1, false);
+      }
+      array.splice(i, 1, false);
+    }
+
+    if (array[i] === '--double-next') {
+      array.splice(i, 1, array[i + 1]);
+    }
+
+    if (array[i] === '--double-prev') {
+      array.splice(i, 1, array[i - 1]);
+    }
+  }
+  return array.filter((e) => e);
 }
 
 module.exports = {
